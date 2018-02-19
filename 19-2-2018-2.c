0626a981ccacc50d1ad9e54b949e6ce8f6a5bff8
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-19 15:57:10 +0530

    Create 19-2-2018-2.c

---
+++ 19-2-2018-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int n
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<=n/2;i++)
+{
+if(n% ;i;0)
+{
+flag
+}
+}if(flag=0)
+printf("enter the prime number");
+return 0;
+}
